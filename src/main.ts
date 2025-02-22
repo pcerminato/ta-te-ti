08244a92de0e59a7f3aa9f604e2bd8f8106b4410
pcerminato@gmail.com <pcerminato@gmail.com>
2025-02-22 18:07:36 +0100

    feat: Initializes view events

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import "./style.css";
 import { gameStart } from "./ui/index.ts";
 
-// @ts-expect-error
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="board">
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
-    <div class="seat"></div>
+    <div class="seat" data-seat-index="0"></div>
+    <div class="seat" data-seat-index="1"></div>
+    <div class="seat" data-seat-index="2"></div>
+    <div class="seat" data-seat-index="3"></div>
+    <div class="seat" data-seat-index="4"></div>
+    <div class="seat" data-seat-index="5"></div>
+    <div class="seat" data-seat-index="6"></div>
+    <div class="seat" data-seat-index="7"></div>
+    <div class="seat" data-seat-index="8"></div>
   </div>
 `;
 
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,52 @@
 import { Game } from "../core/Game.ts";
+import { Board, Move } from "../types/index.ts";
+
+const UNKNOWN_ERR = "Unknown error";
 
 export function gameStart() {
-  return "game start";
+  const game = new Game();
+  const boardView = document.querySelector("div.board");
+  const handleBoardClick = boardClickEventHandler(game);
+
+  boardView?.addEventListener("click", handleBoardClick);
+
+  return game;
+}
+
+function logGameState(stateMessage: string) {
+  console.log(stateMessage);
+}
+
+function drawBoard(board: Board) {
+  console.log(board);
+}
+
+function boardClickEventHandler(game: Game) {
+  return function handleBoardClick(event: Event) {
+    const { target } = event;
+    const { seatIndex } = (target as HTMLDivElement).dataset;
+
+    const symbol = game.getNextTurnSymbol();
+    const position = parseInt(seatIndex || "0");
+    const move: Move = {
+      position,
+      symbol,
+    };
+
+    try {
+      game.playTurn(move);
+
+      if (game.gameOverCheck()) {
+        // here event.currentTarget = this, but currentTarget is more convenient to type
+        (event.currentTarget as HTMLDivElement).removeEventListener(
+          "click",
+          handleBoardClick,
+        );
+        logGameState("Game over"); // TODO: detailed state (who won?)
+      }
+    } catch (error) {
+      const message = (error as Error)?.message || UNKNOWN_ERR;
+      logGameState(message);
+    }
+  };
 }
